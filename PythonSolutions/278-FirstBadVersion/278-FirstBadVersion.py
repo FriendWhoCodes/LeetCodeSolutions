8fa501a4d64b0a2c45214cb498bbc71bd5f56be1
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-06-24 23:56:28 +0530

    Create 278-FirstBadVersion.py

---
+++ PythonSolutions/278-FirstBadVersion/278-FirstBadVersion.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n 
+        
+        while (left < right) :
+            mid = left + (right - left) // 2    # To avoid Int overflow
+            if(isBadVersion(mid)):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+        
+        
+        
