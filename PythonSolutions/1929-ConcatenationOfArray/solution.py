2c2916ef3df4849b15b3a456b5430e7fa07177c6
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-07-23 19:20:40 +0530

    Create solution.py

---
+++ PythonSolutions/1929-ConcatenationOfArray/solution.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/concatenation-of-array/
+
+
+# Using the built in functionalities:
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        result = nums * 2
+        
+        return result # or simply return nums * 2
+      
+      
+'''      
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+                
+        return nums + nums
+        
+'''        
+        
+        
