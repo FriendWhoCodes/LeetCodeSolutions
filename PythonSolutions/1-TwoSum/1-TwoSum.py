58ae3ac2ec97bcd821704d7560c7a398cc40650d
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-08-02 23:51:50 +0530

    Update 1-TwoSum.py

--- PythonSolutions/1-TwoSum/1-TwoSum.py
+++ PythonSolutions/1-TwoSum/1-TwoSum.py
@@ -8,17 +8,14 @@ Memory Usage: 14.9 MB, less than 39.53% of Python3 online submissions for Two Su
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        solution = []
-        length = len(nums)
-                
-        for i in range(0, length):
-            for j in range(1, length):
-                if (target - nums[i] == nums[j] and i != j):
-                    solution.append(i)
-                    solution.append(j)
-                    return solution
-            
-        return solution
+        
+        n = len(nums)
+        
+        for i in range(0, n):
+            for j in range(1, n):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
+                    
     
     
     '''
