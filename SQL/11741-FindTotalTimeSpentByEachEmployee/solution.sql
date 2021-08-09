d064878b38cfb4e503231c2fa351e7509a241bc7
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-08-09 11:31:41 +0530

    Update solution.sql

--- SQL/11741-FindTotalTimeSpentByEachEmployee/solution.sql
+++ SQL/11741-FindTotalTimeSpentByEachEmployee/solution.sql
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/find-total-time-spent-by-each-employee/
 
-SELECT event_day as day, emp_id, sum(out_time - in_time) as total_time FROM employees GROUP BY day, emp_id
+SELECT event_day as day, emp_id, sum(out_time) - sum(in_time) as total_time FROM employees GROUP BY day, emp_id
