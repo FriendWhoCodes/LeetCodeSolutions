f78d5e655998b4d1a226c20f38367d2dc8fb649f
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-08-20 21:10:50 +0530

    Update 1-TwoSum.py

--- PythonSolutions/1-TwoSum/1-TwoSum.py
+++ PythonSolutions/1-TwoSum/1-TwoSum.py
@@ -1,6 +1,6 @@
 '''
 1. Brute Force Approach: Run two loops and try with all possibilites. Except the cases where the number is repeated 
-(e.g. in [1,5,5], answer is [1,2] and not [1,1] or [2,2]. Same index is not allowed.)
+(e.g. in [1,5,5], answer is [1,2] and not [1,1] or [2,2] i.e. same index is not allowed and i and j must be different)
 
 Runtime: 7736 ms, faster than 5.00% of Python3 online submissions for Two Sum.
 Memory Usage: 14.9 MB, less than 39.53% of Python3 online submissions for Two Sum.
