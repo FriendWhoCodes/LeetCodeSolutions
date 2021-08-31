ba111cc71ba519443fe404b30511054aff1d1cef
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-09-01 00:41:46 +0530

    Update solution.py

--- GFG/PythonPrograms/BasicPrograms/3-FactorialOfNumber/solution.py
+++ GFG/PythonPrograms/BasicPrograms/3-FactorialOfNumber/solution.py
@@ -1,12 +1,29 @@
 # https://www.geeksforgeeks.org/python-program-for-factorial-of-a-number/
 
+# Recursive
 
 def factorial(n):
-  if n == 1 or n == 0:
-    return 1
-  else:
-    return n * factorial(n-1)
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
   
   
 # Driver code
 a = print(factorial(5))
+
+# Iterative
+
+
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        factorial = n
+        while n > 1:
+            factorial = factorial * (n -1)
+            n -= 1
+    return factorial
+  
+# Driver code
+a = print(factorial(6))
