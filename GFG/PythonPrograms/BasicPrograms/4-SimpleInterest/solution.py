1b6fcb7ed3afb7ee93e0350d49ca7ef79e4bd192
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-09-01 00:50:50 +0530

    Create solution.py

---
+++ GFG/PythonPrograms/BasicPrograms/4-SimpleInterest/solution.py
@@ -0,0 +1,9 @@
+# https://www.geeksforgeeks.org/python-program-for-simple-interest/
+
+def simple_interest(principal_amount, rate, time):
+    
+    return (principal_amount * rate * time) / 100
+
+# driver program:
+
+print(simple_interest(1000, 2, 2))
