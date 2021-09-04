f8fe7880444d22c815124fa9fb14abce52230864
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-09-04 08:07:19 +0530

    Update solution.py

--- PythonSolutions/387FirstUniqueCharacterInAString/solution.py
+++ PythonSolutions/387FirstUniqueCharacterInAString/solution.py
@@ -16,3 +16,24 @@ class Solution:
                 return index
         
         return -1
+
+    
+    # Actually Fails since it returns the last occurrence of any chracter: e.g. for "aabb" returns 1 which is the index of last a, even though it's clearly repeated
+    # Bruteforce Approach
+    # Pick a character then check the rest of the string if it exists or not
+    # If it exists, pick the next character else return this character
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        index = -1
+        
+        for i in range(len(s)):
+            exists = False
+            for j in range(i+1, len(s)):
+                if s[j] == s[i]:
+                    exists = True
+            if exists == False:
+                index = i
+                break
+        
+        return index
