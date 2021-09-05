3503b8ceaae186b9b377f478b1975d5c37baad79
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-09-05 20:11:27 +0530

    Update solution.py

--- PythonSolutions/121-BestTimeToBuyAndSellStocks/solution.py
+++ PythonSolutions/121-BestTimeToBuyAndSellStocks/solution.py
@@ -1,5 +1,28 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+from math import inf
+
+# Single pass solution
+# Keep a track of minimum value and max_profit so far.
+# For every element, calculate the difference between that element and minimum of all values so far
+# If the difference found is greater than the difference so far, we update the max profit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0:
+            return 0
+
+        max_profit = 0
+        min_price = inf
+        
+        for i in range(0, len(prices)):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
+        
+        return max_profit
+            
+        
+
 # Bruteforce Solution
 # Time Limit Exceeded 198 / 211 test cases passed.
 
