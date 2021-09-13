102719f28ed586772f5727c6dd291a4d03e7f928
Alok Sharma <manofwisdom@users.noreply.github.com>
2021-09-14 03:16:49 +0530

    Update solution.py

--- PythonSolutions/15-3Sum/solution.py
+++ PythonSolutions/15-3Sum/solution.py
@@ -1,4 +1,46 @@
+# https://leetcode.com/problems/3sum/
 
+# Two pointers solution:
+# First we sort the function, so we can apply the two sum logic here
+# We create a separate function from two sum
+# We call that function for each value of the nums array and calculate the sum with this value and the two pointers
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            elif i == 0 or nums[i] != nums[i - 1]:
+                self.two_pointers_sum(nums, i, result)
+        
+        return result
+    
+    
+    def two_pointers_sum(self, nums, i, result):
+        left = i + 1
+        right = len(nums) - 1
+        
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([ nums[i], nums[left], nums[right] ])
+                left += 1
+                right -= 1
+                
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
 
 
 
