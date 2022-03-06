556ace2ebb71fdf09bfcfe77b771ef22956d6bfa
Alok Sharma <manofwisdom@users.noreply.github.com>
2022-03-06 18:22:06 +0530

    Update solution.py

--- PythonSolutions/1572-MatrixDiagonalSum/solution.py
+++ PythonSolutions/1572-MatrixDiagonalSum/solution.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/matrix-diagonal-sum/
 
+# TODO an O(n) solution
+
+
 # Note how the primary diagonal is simply indices with same value i.e. i == j
 # Note how secondary diagonal is indices with changing value for both i and j. With i from 0 and j from len(mat) - 1 to i increasing to len(mat) - 1 and j decreasing to 0
 # i.e. secondary diagonal goes from mat[0][len(mat)-1] to mat[len(mat) - 1][0].
@@ -29,3 +32,29 @@ class Solution:
         
         return pri_diagonal + sec_diagonal
         
+# Another solution with both diagonals being calculated within one outer while loop
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        
+        rows = len(mat)
+        cols = len(mat[0])
+        
+        i, j = 0, 0
+        dia1 , dia2 = 0, 0
+        k = cols - 1
+        
+        while i < rows and k >= 0:
+            while j < cols:
+                if i == j:
+                    dia1 += mat[i][j]
+                j += 1
+            
+            if i != k:
+                dia2 += mat[i][k]
+            k -= 1
+            j = 0
+            i += 1
+        
+        return dia1 + dia2
+            
